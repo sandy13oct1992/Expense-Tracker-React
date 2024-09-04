c625731dcd4bafda5465777b9aff836e6098c0ab
sandy13oct1992 <sandy13oct1992@gmail.com>
2024-09-04 21:47:50 +0530

    expense tracker

--- src/component/UpdateProfile.js
+++ src/component/UpdateProfile.js
@@ -1,14 +1,33 @@
+import { useState } from "react"
 
 
 const UpdateProfile = () =>{
+    const [name, setName] =useState('');
+    const [photoURL, setPhotoURL] = useState('');
 
+    const nameChangeHandler = (e) => {
+     setName(e.target.value);
+    }
+
+    const urlChangeHandler = (e) => {
+    setPhotoURL(e.target.value);
+    }
+
+  const handleUpdation = () => {
+    const dataObj = {
+        name : name,
+        photoURL: photoURL,
+        id:Date.time(),
+    }
+    // const response = axios.
+  }
     return(
         <div>
             <h1>Contact Details</h1>
             <label>Full Name:</label>
-            <input type='text' />
+            <input type='text' value={name} onChange={nameChangeHandler} />
             <label>Profile Photo URL</label>
-            <input type='text'/>
+            <input type='text'value={photoURL} onChange={urlChangeHandler}/>
             <button>Update</button>
             <button>Cancel</button>
         </div>
